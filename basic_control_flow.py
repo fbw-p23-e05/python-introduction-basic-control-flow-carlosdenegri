b7619d95d25521df49660ef8a35cb4d0f8a0fcac
Carlos Adrian Denegri Martinez <cadenegr@c4c1c3.42berlin.de>
2023-08-23 16:29:05 +0200

    adding tasks

---
+++ basic_control_flow.py
@@ -0,0 +1,73 @@
+#Task 1 basic math operations
+
+# print("Task1")
+# i = 0;
+# number = int;
+# while i < 3:
+# 	number = int(input("Enter number: "))
+# 	if (number & 1) != 0:
+# 		print("Odd number")
+# 	else:
+# 		print("Even number")
+# 	i = i + 1
+
+#Task 2 calculate the sum of the numbers
+
+# print("Task2")
+# i = 0
+# number = int
+# while i < 3:
+# 	number = int(input("enter number: "))
+# 	number = number + number
+# 	if i == 2:
+# 		print("The sum of the numbers is: ", number)
+# 	i = i + 1
+
+#Task 3 Find the maximum number
+
+# print("Task3")
+# i = 0
+# numbers = []
+# for i in range(5):
+# 	number = int(input("Enter number: "))
+# 	numbers.append(number)
+
+# 	print("The maximum number is: ", max(numbers))
+
+#Task 4 Find divisors of a number
+
+# print("Task4")
+
+# number = int(input("Enter number: "))
+# divisors = []
+
+# for i in range(1, number + 1): #technique used in loops to iterate over a range of numbers
+# 	if number % i == 0:
+# 		divisors.append(i)
+
+# print(f"The divisors of {number}:") #f-string formatting. Serves to print the variabel within{}
+# for i in divisors: #Like in C while(str[i]) then i++;
+# 	print(i)
+
+#Task 5 Check if a number is even and divisible by 3
+
+# print("Task5")
+# number = int(input("Enter number: "))
+# if (number % 2 == 0) and (number % 3 == 0):
+# 	print(f"The {number} is even and divisable by 3")
+# else:
+# 	print(f"The number {number} Does not apply for this function!")
+
+#Task 6 Check if a number is positive, odd and divisiable by 7
+
+# print("Task6")
+# number = int(input("Enter number: "))
+# if (number > 0) and (number % 2 != 0) and (number % 7 == 0):
+# 	print(f"{number} is positive, odd and divisible by 7")
+# else:
+# 	print(f"The number {number} Does not apply for this function!")
+
+
+
+
+
